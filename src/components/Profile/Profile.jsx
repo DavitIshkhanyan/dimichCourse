dcf133070d4ccc378d4cd5cf4d4790986dd06941
Davit Ishkhanyan Karen <davit.ishkhanyan.k@digitain.com>
2022-07-04 18:27:51 +0400

    second commit

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,8 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
-    return <div className={s.content}>
+    return (
+    <div>
         <div>
             <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/
 Landscape-Color.jpg' />
@@ -12,6 +13,7 @@ Landscape-Color.jpg' />
         </div>
         <MyPosts />
     </div>
+    );
 }
 
 export default Profile;
